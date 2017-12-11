3678f65d9761455aa9542bad8f827f657fcf3045
Mark <Mark@DESKTOP-QF0L5TF>
2017-12-11 21:15:25 +0100

    added the start of jazz

---
+++ WebAppHF/Repositories/JazzRepo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebAppHF.Models;
+
+namespace WebAppHF.Repositories
+{
+    public class JazzRepo : IJazzRepo
+    {
+        public List<Jazz> GetAll()
+        {
+            IEnumerable<Jazz> Jazzs;
+            using (HFContext context = new HFContext())
+            {
+                Jazzs = context.Jazzs.AsEnumerable();
+            }
+
+            return Jazzs.ToList();
+        }
+
+        public Jazz GetJazzByID(int ID)
+        {
+            Jazz jazz;
+            using (HFContext context = new HFContext())
+            {
+                jazz = context.Jazzs.SingleOrDefault(j => j.ID == ID);
+            }
+            return jazz;
+        }
+    }
+}
