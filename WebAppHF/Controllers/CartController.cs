cd52148133d4dc69852b5ceaa5fae46621fde75b
Hossam Chakroun <hossam-98@live.nl>
2018-01-14 00:05:04 +0100

    yihub

---
+++ WebAppHF/Controllers/CartController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebAppHF.Models;
+
+namespace WebAppHF.Controllers
+{
+    public class CartController : Controller
+    {
+        // GET: Cart
+        public ActionResult Index()
+        {
+            CartModel cart = (CartModel)Session["Cart"];
+            return View(cart);
+        }
+
+        [HttpPost]
+        public ActionResult Index(CartModel cart)
+        {
+            //insert
+
+            //Redirect success/failure page
+            return View();
+        }
+    }
+}
---
+++ WebAppHF/Models/CartModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebAppHF.Enums;
+
+namespace WebAppHF.Models
+{
+    public class CartModel
+    {
+        public List<Event> Items { get; set; }
+        public PaymentMethod PaymentMethod { get; set; }
+    }
+}
