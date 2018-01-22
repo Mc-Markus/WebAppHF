954a101ac3203b30533e49bbbf6ba8b357d5b75b
Mark <Mark@DESKTOP-QF0L5TF>
2018-01-22 10:38:21 +0100

    Added some comments in the code

--- WebAppHF/Models/JazzBook.cs
+++ WebAppHF/Models/JazzBook.cs
@@ -11,16 +11,12 @@ namespace WebAppHF.Models
         public DisplayRecord WeekendPassePartout { get; set; }
         public List<DisplayRecord> DayEvents { get; set; }
 
+        //added this class to have all essential parts for the jazz booking page
         public JazzBook(DisplayRecord day, DisplayRecord weekend, List<DisplayRecord> events)
         {
             this.DayPassePartout = day;
             this.WeekendPassePartout = weekend;
             this.DayEvents = events;
         }
-    }
-
-    public class TourDay
-    {
-         
     }
 }
