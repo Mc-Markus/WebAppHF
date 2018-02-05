9946271bd501baea6106330171b04b3f7f8c3b52
Mark <Mark@DESKTOP-QF0L5TF>
2018-02-05 22:35:01 +0100

    Restored my cart for testing purposes

--- WebAppHF/Controllers/CartController.cs
+++ WebAppHF/Controllers/CartController.cs
@@ -19,101 +19,105 @@ namespace WebAppHF.Controllers
         IRestaurantRepo restaurantRepo = new RestaurantRepo();
 
         // GET: Cart
-//        public ActionResult Index()
-//        {
-//            //Checks if sessions contains a valid list of records
-//            //List<Record> sessionCart = null;
-//            //try
-//            //{
-//            //    sessionCart = (List<Record>)Session["Cart"];
-//            //}
-//            //catch
-//            //{
-//            //    return RedirectToAction("CartEmpty");
-//            //}
-//            ////Checks if cart isnt empty
-//            //if (sessionCart == null)
-//            //{
-//            //    return RedirectToAction("CartEmpty");
-//            //}
+        public ActionResult Index()
+        {
+            //Checks if sessions contains a valid list of records
+            List<Record> sessionCart = null;
+            try
+            {
+                sessionCart = (List<Record>)Session["Cart"];
+            }
+            catch
+            {
+                return RedirectToAction("CartEmpty");
+            }
+            //Checks if cart isnt empty
+            if (sessionCart == null)
+            {
+                return RedirectToAction("CartEmpty");
+            }
 
-//            ////creates displayRecords for all records in session
-//            //List<DisplayRecord> displayRecords = new List<DisplayRecord>();
-//            //foreach (Record record in sessionCart)
-//            //{
-//            //    displayRecords.Add(new DisplayRecord(getEvent(record), record));
-//            //}
+            //creates displayRecords for all records in session
+            List<DisplayRecord> displayRecords = new List<DisplayRecord>();
+            foreach (Record record in sessionCart)
+            {
+                displayRecords.Add(new DisplayRecord(getEvent(record), record));
+            }
 
-//            ////chooses 3 random restaurants for cross selling
-//            //List<Restaurant> restaurants = restaurantRepo.GetAllRestaurants().ToList();
+            //chooses 3 random restaurants for cross selling
+            List<Restaurant> restaurants = restaurantRepo.GetAllRestaurants().ToList();
 
-//            //Random rnd = new Random();
+            Random rnd = new Random();
 
-//            //List<Restaurant> crossSelling = new List<Restaurant>();
+            List<Restaurant> crossSelling = new List<Restaurant>();
 
-//            //for (int i = 0; i < 3; i++)
-//            //{
-//            //    Restaurant restaurant = restaurants[rnd.Next(0, restaurants.Count())];
-//            //    crossSelling.Add(restaurant);
-//            //    restaurants.Remove(restaurant);
-//            //}
+            for (int i = 0; i < 3; i++)
+            {
+                Restaurant restaurant = restaurants[rnd.Next(0, restaurants.Count())];
+                crossSelling.Add(restaurant);
+                restaurants.Remove(restaurant);
+            }
 
-//            //CartViewModel cartViewModel = new CartViewModel(displayRecords, crossSelling);
+            CartViewModel cartViewModel = new CartViewModel(displayRecords, crossSelling);
 
-//            if (Session["Cart"] == null && Session["RestCart"] == null)
-//            {
-//                return RedirectToAction("CartEmpty");
-//            }
-//            else
-//            {
-//                CartModel cart = new CartModel();
-//                cart.Items = (List<Event>)Session["cart"];
-//                cart.RestItems = (List<Restaurant>)Session["RestCart"];
-//                //List<Event> list = rep.GetEvents();
-//                //cart.Items = list;
+            return View(cartViewModel);
 
-//                //////Crossselling
-//                //Random rng = new Random();
-//                //List<Event> cross = new List<Event>();
+            #region old cart hossam
+            //if (Session["Cart"] == null && Session["RestCart"] == null)
+            //{
+            //    return RedirectToAction("CartEmpty");
+            //}
+            //else
+            //{
+            //    CartModel cart = new CartModel();
+            //    cart.Items = (List<Event>)Session["cart"];
+            //    cart.RestItems = (List<Restaurant>)Session["RestCart"];
+            //List<Event> list = rep.GetEvents();
+            //cart.Items = list;
 
-//                //int i = 0;
+            //////Crossselling
+            //Random rng = new Random();
+            //List<Event> cross = new List<Event>();
 
-//                //while (i != 5)
-//                //{
-//                //    cross.Add(list[rng.Next(0, (list.Count - 1))]);
-//                //    i++;
-//                //}
+            //int i = 0;
 
-//                //cart.CrossSellItems = cross;
-//                int totalPrice = 0;
+            //while (i != 5)
+            //{
+            //    cross.Add(list[rng.Next(0, (list.Count - 1))]);
+            //    i++;
+            //}
 
-//                foreach (Event e in cart.Items)
-//                {
-//                    if (e is TalkModel)
-//                    {
-//                        TalkModel talk = (TalkModel)e;
-//                        totalPrice += (talk.Amount * talk.Price);
-//                    }
-//                    //zet hier jullie viewmodels van jullie champions die een amount bevat net als de if statement hierboven.
-                    
-                    
-//                }
+            //cart.CrossSellItems = cross;
+            //int totalPrice = 0;
 
-//                foreach (Restaurant rest in cart.RestItems)
-//                {
-//                    //doe hetzelfde hier met een viewmodel met een amount voor restaurant als hierboven.
-//                }
-//            { 
-//}
-//                return View(cart);
-//            }
-//        }
+            //foreach (Event e in cart.Items)
+            //{
+            //    if (e is TalkModel)
+            //    {
+            //        TalkModel talk = (TalkModel)e;
+            //        totalPrice += (talk.Amount * talk.Price);
+            //    }
+            //    //zet hier jullie viewmodels van jullie champions die een amount bevat net als de if statement hierboven.
 
-//        [HttpPost]//Cartmodel is no longer in use
-//        public ActionResult Index(CartModel cart)
-//        {
-//            return View();
-//        }
+
+            //}
+
+            //foreach (Restaurant rest in cart.RestItems)
+            //{
+            //    //doe hetzelfde hier met een viewmodel met een amount voor restaurant als hierboven.
+            //}
+            //{
+            //}
+            //return View(cart);
+            //}
+            #endregion
+        }
+
+        [HttpPost]//Cartmodel is no longer in use
+        public ActionResult Index(CartModel cart)
+        {
+            return View();
+        }
 
         public ActionResult Cart()
         {
@@ -193,21 +197,21 @@ namespace WebAppHF.Controllers
         }
 
         //Gets event based on eventType and eventID
-        //public Event getEvent(Record record)
-        //{
-        //    switch (record.EventType)
-        //    {
-        //        case "Jazz":
-        //            return jazzRepo.GetJazzByID(record.EventID);
-        //        case "Tour":
-        //            return tourRepo.GetWalkByID(record.EventID);
-        //        case "Talk":
-        //            return talkRepo.GetTalk(record.EventID);
-        //        case "RESTAURANTSESSION":
-        //            return restaurantSession.GetRestaurantByID(record.EventID);
-        //        default:
-        //            return null;
-        //    }
-        //}
+        public Event getEvent(Record record)
+        {
+            switch (record.EventType)
+            {
+                case "Jazz":
+                    return jazzRepo.GetJazzByID(record.EventID);
+                case "Tour":
+                    return tourRepo.GetWalkByID(record.EventID);
+                case "Talk":
+                    return talkRepo.GetTalk(record.EventID);
+                case "RESTAURANTSESSION":
+                    //return restaurantSession.GetRestaurantByID(record.EventID);
+                default:
+                    return null;
+            }
+        }
     }
 }
