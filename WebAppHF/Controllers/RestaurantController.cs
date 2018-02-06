e2c8076ab9c4d8f9915716dd9ad94eb0a5e25d32
Samir Yeasin <570495@student.inholland.nl>
2018-02-06 06:25:37 +0100

    reservering remastered

--- WebAppHF/Controllers/RestaurantController.cs
+++ WebAppHF/Controllers/RestaurantController.cs
@@ -11,7 +11,11 @@ namespace WebAppHF.Controllers
     public class RestaurantController : Controller
     {
         // Maak instantie van de interface om niet direct met je database te praten
+        private IRecordRepository recordRepository = new RecordRepository();
         private IRestaurantRepo restaurantRepo = new RestaurantRepo();
+        private HFContext db = new HFContext();
+        // Hardcode eventtype to give to the record eventId
+        private string eventType = "RestaurantSession";
 
         // GET: Restaurant
         // Toon in een lijst alle restauranten
@@ -67,21 +71,6 @@ namespace WebAppHF.Controllers
             //return View();
         }
 
-
-
-        //Toont een specifiek restaurant in de view door middel van een ID
-        // Dit is de reserveringspagina
-        public ActionResult Detail(int ID)
-        {
-            // Zoekt voor een restaurant met die ID
-            Restaurant restaurant = restaurantRepo.GetRestaurant(ID);
-            // Restaurants die niet bestaan worden gestuurd naar de PageNotFound in de homecontroller
-            if (restaurant == null)
-                return RedirectToAction("PageNotFound", "Home");
-            
-            return View(restaurant);
-        }
-
         //Toont een specifiek restaurant in de view door middel van een ID
         // Dit is de extra informatie pagina
         public ActionResult AfterDetail(int ID)
@@ -93,6 +82,67 @@ namespace WebAppHF.Controllers
                 return RedirectToAction("PageNotFound", "Home");
 
             return View(restaurant);
+        }
+
+        [HttpGet]
+        public ActionResult MakeReservation(int id)
+        {
+            // Giving the information about the restaurant
+            Restaurant restaurant = new Restaurant();
+            restaurant = restaurantRepo.GetRestaurant(id);
+
+            // Creating two dropdowns to pick from 
+            // First one is Day
+            List<DateTime> Day = restaurantRepo.GetAllDayList(id);
+            ViewBag.selectTestDayList = new SelectList(Day, "Date");
+
+            // Second one is Time 
+            List<DateTime> Time = restaurantRepo.GetAllTimeList(id);
+            ViewBag.selectTestTimeList = new SelectList(Time, "StartTime");
+
+            //Create new Record to use in the View
+            DisplayRecord record = new DisplayRecord();
+    
+
+            //Passing viewmodel to the View
+            ReservationVM vm = new ReservationVM(restaurant, Day, Time, record);
+            return View(vm);
+        }
+
+        [HttpPost]
+        public ActionResult AddToDataBase(ReservationVM MyTestParameter)
+        {
+            ReservationVM reservation = MyTestParameter;
+
+            // Giving the record value of eventid and eventtype
+            reservation.Record.Record.EventType = eventType;
+            reservation.Record.Record.EventID = recordRepository.GetEventID(reservation.Restaurant.ID, reservation.Record.Event.Date, reservation.Record.Event.StartTime);
+
+            if (ModelState.IsValid)
+            {
+                List<Record> sessionBasket = new List<Record>();
+                sessionBasket.Add(reservation.Record.Record);
+
+                //check if session contains records if so add them to new cart value
+                try
+                {
+                    List<Record> basket = (List<Record>)Session["Cart"];
+                    foreach (Record sessionrecord in basket)
+                    {
+                        sessionBasket.Add(sessionrecord);
+                    }
+                }
+                catch
+                {
+                    Session["Cart"] = null;
+                }
+                finally
+                {
+                    Session["Cart"] = sessionBasket;
+                }
+            }
+            //send user to basket after things are added to basket
+            return RedirectToAction("Index", "Cart");
         }
     }
 }
