4bd158e8af3668bab35e37e671ec9252c9f451b6
mc-markus <gwn_m4rk@icloud.com>
2018-04-12 20:25:56 +0200

    geprobeerd om passepartout in db te krijgen

---
+++ WebAppHF/Repositories/PassePartoutRepo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebAppHF.Models;
+
+namespace WebAppHF.Repositories
+{
+    public class PassePartoutRepo : IPassePartoutRepo
+    {
+        public PassePartout getWeekendPassePartout()
+        {
+            return null;
+        }
+    }
+}
